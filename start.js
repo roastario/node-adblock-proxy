7395b82b6bbd556b067f98c804a26220fb375862
Stefano Franz <stefano.franz@byhiras.com>
2015-01-15 18:28:24 +0000

    Add ability to specify a specific ip for use in server environments

--- start.js
+++ start.js
@@ -1,50 +1,57 @@
 #!/usr/bin/nodejs
 
 
-(function() {
+(function () {
+    var AdBlockProxy = require('./lib/proxy.js');
+    /*
+     * GET ROOT FOLDER
+     */
 
-	/*
-	 * GET ROOT FOLDER
-	 */
+    var root = null;
+    var port = 8080;
+    var tmp = null;
+    var host = null;
 
-	var root = null;
-	var port = 8080;
-	var tmp  = null;
+    var file = __filename.split('/');
+    if (file.pop() === 'start.js') {
+        root = file.join('/');
+    }
 
-	var file = __filename.split('/');
-	if (file.pop() === 'start.js') {
-		root = file.join('/');
-	}
-
-	if (root === null) {
-		root = __dirname || null;
-	}
-
-
-	/*
-	 * GET SETTINGS
-	 */
-
-	if (process.argv instanceof Array) {
-
-		if (typeof process.argv[2] === 'string') {
-			tmp  = parseInt(process.argv[2], 10);
-			port = !isNaN(tmp) ? tmp : port;
-		}
-
-	}
+    if (root === null) {
+        root = __dirname || null;
+    }
 
 
-	var _Proxy = require('./lib/proxy.js');
+    /*
+     * GET SETTINGS
+     */
+
+    if (process.argv instanceof Array) {
+
+        if (process.argv.length === 4) {
+            host = process.argv[2];
+            port = parseInt(process.argv[3], 10);
+        } else if (process.argv.length === 3) {
+            console.info('As no host was specified, the default \'localhost\' host will be used');
+            host = 'localhost';
+            port = parseInt(process.argv[2], 10);
+        } else {
+            console.error('usage: nodejs start.js [host] <port>');
+            process.exit(255);
+        }
+
+    }
 
 
-	var proxy = new _Proxy({
-		root: root
-	});
+    var proxy = new AdBlockProxy({
+        root: root,
+        host: host,
+        port: port
+    });
 
 
-	proxy.listen('http',   port);
-	proxy.listen('socks5', 1080);
+    proxy.listen('http', port);
+    proxy.listen('socks5', 1080);
 
 })();
 
